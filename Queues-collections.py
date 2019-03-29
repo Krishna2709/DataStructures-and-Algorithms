5b5564c6b46bd97d3b0731c0f0b8efc10277b910
Krishna2709 <krishnakankipati@icloud.com>
2019-03-29 20:57:56 +0530

    Queue - from collections library using deque package

---
+++ Queues-collections.py
@@ -0,0 +1,8 @@
+from collections import dque
+
+queue = deque(['Bill','Elon','Steve'])
+queue.append('Warren')
+queue.append('Brin')
+queue.popleft()
+
+# Lookup = = > ' https://docs.python.org/3/tutorial/datastructures.html ' < = =
